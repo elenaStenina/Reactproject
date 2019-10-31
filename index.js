e2ee63e2bc8ecafb0378bf54e83d59df4230a6d7
elenaStenina <49044678+elenaStenina@users.noreply.github.com>
2019-10-31 13:52:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'babel-polyfill';
 import ReactDOM from "react-dom";
 import App from "./component/App";
-import "./index.css";
+import "./index.scss";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 ReactDOM.render(
